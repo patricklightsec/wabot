49d1cab6bfd3cc6811ee4179ed30d40c32e73cc4
Naze-dev <78690561+Naze-dev@users.noreply.github.com>
2021-02-20 21:17:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6288228825906'] // Put your number here
+global.owner = ['6285875158363'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = ['6288228825906'] // Premium user has unlimited limit
+global.prems = ['6285875158363'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
