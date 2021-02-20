b40b2ddacb041ce80bbc03f88447e9fa7f0a1c00
Naze-dev <78690561+Naze-dev@users.noreply.github.com>
2021-02-20 21:20:34 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6288228825906', 'Naze', m)
-  this.sendContact(m.chat, '6288228825906', 'Naze', m)
+  // this.sendContact(m.chat, '6285875158363', 'Nazeda', m)
+  this.sendContact(m.chat, '6285875158363', 'Nazeda', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
