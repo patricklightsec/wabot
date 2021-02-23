9b65803eb949ef65ea59261a7d8b30c2ab385edf
Naze-dev <78690561+Naze-dev@users.noreply.github.com>
2021-02-23 20:16:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,9 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa • Gopay • Dana 」
+╭─「 Donasi • Pulsa • Gopay • Dana • Subscribe 」
 │ • Smartfren [088228825906]
 │ • Gopay [088228825906]
 │ • Dana [088228825906]
+│ • Youtube [https://youtube.com/channel/UCl77jQD3nSFp__z1oRxm-fA]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
